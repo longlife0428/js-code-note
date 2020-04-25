e1e94e3fd472a13f746330fa4f91db64ce89dce2
longlife0428 <longlife0428@gmail.com>
2020-04-26 01:23:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
-// start learning javascript for a while, used to be just css designer... just crash into h5 game last summer!!
-// I met html since almost 15 years. so far I have learned javascript, jqeury, some framework like mostly bootstrap, vue, smart..smth
-// Work with backend with custom RESTFull API, just started to learn socket:)
+//var time = "2020-04-25T18:00:00";
+var countDownDate = ["Jan 5, 2021 15:37:25", "Aug 5, 2021 15:37:25", "Feb 12, 2021 15:37:25"]
+//alert(countDownDate)
+
+function returnTime ($time, $id){
+	var countDate = new Date($time).getTime();
+    // Set the date we're counting down to
+    // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	
+      // Output the result in an element with id="demo"
+      var result = days + "d " + hours + "h "  + minutes + "m " + seconds + "s ";
+      $("#div"+$id).text(result)
+      //console.log($id)
+      // If the count down is over, write some text 
+      if (distance < 0) {
+        clearInterval(x);
+        //document.getElementById("demo").innerHTML = "EXPIRED";
+      }
+    }, 1000);
+	
+}
+function init(){
+  //returnTime(countDownDate[0])
+    $("#timer").html("");
+    for(var i =0 ; i<countDownDate.length; i++ ){
+      var res = 
+      '<img src="https://lh3.googleusercontent.com/proxy/xLOQKM2aCMvmtU4cUeq3YKUKG-tG_l1UFCQ5Fd05MxbkbaT7zpFm7DUglwixFhJSTNhJ0YbjZteCcqqy46_CFh6FM6WorFKJWwXQOgl_jjThV89rvBGeVrWV5iM9KZU_GgBh2EuZMlvadqSafa6elx1D8YgfAh8-oUhzQpLg_eGZbmy_W7JgBmG7Qbo" width="100" heigh="100" alt=""/>'+
+      '<div id="div'+i+'">'+returnTime(countDownDate[i], i)+'</div>';
+      $("#timer").append(res)
+    }
+
+}
+
+$(document).ready(function(){
+  $("p").click(function(){
+    $(this).hide();
+  });
+  init()
+});
