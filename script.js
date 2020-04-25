7a4c79bca2ea96e70941b4621e70e2421b80d7f1
longlife0428 <longlife0428@gmail.com>
2020-04-25 17:33:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+// start learning javascript for a while, used to be just css designer... just crash into h5 game last summer!!
+// I met html since almost 15 years. so far I have learned javascript, jqeury, some framework like mostly bootstrap, vue, smart..smth
+// Work with backend with custom RESTFull API, just started to learn socket:)
